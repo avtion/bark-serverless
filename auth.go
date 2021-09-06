65bd960174591e489119f97e45b7db6c3e2608bf
Avtion <mail@avtion.cn>
2021-09-06 22:30:15 +0800

    feature: 支持basic auth

--- auth.go
+++ auth.go
@@ -13,17 +13,19 @@ func authMiddleware() gin.HandlerFunc {
 	const defaultRealm = "Coffee Time"
 
 	var accounts = make(map[string]string)
-	for _, env := range os.Environ() {
-		tmp := strings.SplitN(env, "=", 2)
+	for _, v := range os.Environ() {
+		tmp := strings.SplitN(v, "=", 2)
 		user, pw := tmp[0], tmp[1]
 		if !strings.HasPrefix(user, "user_") {
 			continue
 		}
 		accounts[strings.TrimPrefix(user, "user_")] = pw
 	}
+
 	if len(accounts) == 0 {
-		zap.L().Info("basic auth is enable, but no account info in env")
+		return nil
 	}
 
+	zap.L().Info("basic auth is enable")
 	return gin.BasicAuthForRealm(accounts, defaultRealm)
 }
